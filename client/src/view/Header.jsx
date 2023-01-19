6aac5c31e2d50f4e0430ebcd2aafcd5ad74e52a9
Chris Hendricks <crshenx@gmail.com>
2023-01-19 00:18:11 -0800

    grid system refine

--- client/src/view/Header.jsx
+++ client/src/view/Header.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import avatarPlaceHolder from "../assets/avatar-placeholder.png";
 function Header() {
   return (
-    <div className="grid grid-cols-6 grid-rows-1 grid-flow-dense">
-      <div className="rounded-xl  col-start-1 row-span-2">
-        <img className="object-cover h-48 w-48" src={avatarPlaceHolder} />
-      </div>
-      <div className="text-xl font-medium text-black  justify-self-start flex flex-col">
+    <div className="grid grid-cols-12 grid-rows-2 ">
+      <img className="col-span-2 row-span-2" src={avatarPlaceHolder} />
+
+      <div className="text-xl font-medium text-black  justify-self-start flex flex-col col-span-2">
         <div>Client Name</div>
         <div className="justify-self">Full Stack Software Engineer</div>
       </div>
-      <div className="text-slate-500 row-start-1 col-start-3 col-span-3 row-span-2">
+      <div className="text-slate-500 row-start-1 col-start-5 col-span-6 row-span-2">
         A description of the client: Lorem ipsum dolor sit amet, consectetur
         adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
         magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
@@ -19,7 +18,7 @@ function Header() {
         pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
         qui officia deserunt mollit anim id est laborum.
       </div>
-      <div className="col-span-1 justify-self-end col-start-6">
+      <div className="col-span-1 justify-self-end col-start-11 col-span-2">
         Socials or maybe a darkmode toggle button
       </div>
     </div>
