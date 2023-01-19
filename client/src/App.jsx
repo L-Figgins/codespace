766c0c7de4707760a31a613c72e3b603be38cf37
Chris Hendricks <crshenx@gmail.com>
2023-01-18 22:16:13 -0800

    init header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
-
+import Header from "./view/Header";
 function App() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
