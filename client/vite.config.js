45c612e105cfda0c357d6625be5859ee20a1b061
Logan Figgins <l.figgins.se@gmail.com>
2023-01-23 14:57:14 -0800

    update vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+import dns from "dns";
+
+dns.setDefaultResultOrder("verbatim");
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
     port: 3000,
+    strictPort: true,
   },
 });
