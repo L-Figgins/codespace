31d60455f5b3ef8ca1a7170c15be1904df90b798
Logan Figgins <l.figgins.se@gmail.com>
2023-01-24 14:30:40 -0800

    add tests for redis create_user query and fix detected bugs

--- api/codespace_backend/queries/users.py
+++ api/codespace_backend/queries/users.py
@@ -2,34 +2,52 @@ from ..db import get_db
 from ..util import generate_user_id
 from .keys import usernames_unique_key, usernames_key, users_key
 
-def create_user(user:dict):
+
+def create_user(user: dict):
+    """
+    Creates a new user with the given information and stores it in redis as a
+    series of data structures to fascilitate queries.
+
+    :param user: A dictionary containing the user's information, including
+                 'username', 'name', 'password', and 'contactInfo'
+    :return: A string representing the user's unique ID
+
+    :raises ValueError: if the provided username is already taken
+    """
+
     # r = redis
     r = get_db()
 
     user_id = generate_user_id()
 
-    exists = r.sismember(usernames_unique_key(),user["username"])
+    exists = r.sismember(usernames_unique_key(), user["username"])
 
     if exists:
         raise ValueError("Username is taken")
 
-
     pipe = r.pipeline()
-    pipe.hset(users_key(id),serialize(user))
+    pipe.hset(users_key(user_id), serialize(user))
     pipe.sadd(usernames_unique_key(), user["username"])
-    pipe.zadd(usernames_key(),{[user["username"]]: id} )
+    pipe.zadd(usernames_key(), {user["username"]: user_id})
 
     _ = pipe.execute()
 
     return user_id
 
 
-def serialize(user:dict):
+def serialize(user: dict) -> dict:
+    """
+    Serializes the user information into a dictionary of specific fields.
+
+    :param user: A dictionary containing the user's information
+    :return: A dictionary containing the 'username', 'name', 'password', and 'email' fields
+
+    """
     hash = {
         "username": user["username"],
         "name": user["name"],
         "password": user["password"],
-        "email": user["contactInfo"]["email"]
+        "email": user["contactInfo"]["email"],
     }
 
     return hash
---
+++ api/tests/test_queries.py
@@ -0,0 +1,101 @@
+import pytest
+from codespace_backend.queries.users import create_user, serialize
+from codespace_backend.db import get_db
+from codespace_backend.util import generate_user_id
+from codespace_backend.queries.keys import (
+    usernames_unique_key,
+    usernames_key,
+    users_key,
+)
+
+
+@pytest.fixture(name="mock_pipe")
+def mock_pipe(mocker):
+    mock_pipe = mocker.MagicMock()
+    yield mock_pipe
+
+
+@pytest.fixture(name="mock_r")
+def mock_redis(mocker, mock_pipe):
+    mock_r = mocker.MagicMock()
+    mock_r.pipeline.return_value = mock_pipe
+    yield mock_r
+
+
+@pytest.fixture(name="mock_user_id")
+def mock_user_id():
+    uid = "cce8594a-0e36-467d-9dd3-efe9c8376c94"
+    yield uid
+
+
+def test_create_user_success(mocker, mock_r, mock_pipe, mock_user_id):
+    mock_r.sismember.return_value = False
+    # Patch the get_db function to return the mock redis object
+    mocker.patch("codespace_backend.queries.users.get_db", return_value=mock_r)
+    # Patch the generate_user_id function to return a fixed value
+    mocker.patch(
+        "codespace_backend.queries.users.generate_user_id", return_value=mock_user_id
+    )
+
+    user = {
+        "username": "mock_username",
+        "name": "mock_name",
+        "password": "mock_password",
+        "contactInfo": {"email": "mock@email.com"},
+    }
+    # Call the create_user function
+    user_id = create_user(user)
+
+    # assert is Member is called with correct args
+    mock_r.sismember.assert_called_with(usernames_unique_key(), user["username"])
+
+    # Assert that the hset, sadd, and zadd methods were called on the pipeline object
+    mock_pipe.hset.assert_called_with(users_key(mock_user_id), serialize(user))
+    mock_pipe.sadd.assert_called_with(usernames_unique_key(), "mock_username")
+    mock_pipe.zadd.assert_called_with(usernames_key(), {"mock_username": mock_user_id})
+    # Assert that the execute method was called on the pipeline object
+    mock_pipe.execute.assert_called()
+    # Assert that the returned user_id is the expected value
+    assert user_id == mock_user_id
+
+
+def test_create_user_username_taken(mocker, mock_r, mock_pipe, mock_user_id):
+    # Patch the get_db function to return the mock redis object
+    mocker.patch("codespace_backend.queries.users.get_db", return_value=mock_r)
+    # Patch the generate_user_id function to return a fixed value
+    mocker.patch(
+        "codespace_backend.queries.users.generate_user_id", return_value=mock_user_id
+    )
+    # Set the sismember return value to True, indicating that the username is taken
+    mock_r.sismember.return_value = True
+
+    user = {
+        "username": "mock_username",
+        "name": "mock_name",
+        "password": "mock_password",
+        "contactInfo": {"email": "mock@email.com"},
+    }
+
+    # Assert that calling create_user function raises ValueError
+    with pytest.raises(ValueError):
+        create_user(user)
+    # Assert that the hset, sadd, and zadd methods were not called
+    mock_pipe.hset.assert_not_called()
+    mock_pipe.sadd.assert_not_called()
+    mock_pipe.zadd.assert_not_called()
+
+
+def test_serialize():
+    user = {
+        "username": "mock_username",
+        "name": "mock_name",
+        "password": "mock_password",
+        "contactInfo": {"email": "mock@email.com"},
+    }
+    expected_output = {
+        "username": "mock_username",
+        "name": "mock_name",
+        "password": "mock_password",
+        "email": "mock@email.com",
+    }
+    assert serialize(user) == expected_output
