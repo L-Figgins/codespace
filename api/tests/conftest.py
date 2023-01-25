e7606416ab1b9ee5db965cfec6ef50200a6e4324
Logan Figgins <l.figgins.se@gmail.com>
2023-01-25 15:46:34 -0800

    factor out fixtures

---
+++ api/tests/conftest.py
@@ -0,0 +1,36 @@
+import pytest
+import redis
+from os import getenv
+
+
+@pytest.mark.integration
+@pytest.fixture(name="redis")
+def real_redis():
+    r = redis.Redis(
+        host=getenv("REDIS_HOST", ""),
+        port=getenv("REDIS_PORT", "6379"),
+        password=getenv("REDIS_PW", "devpassword"),
+        decode_responses=True,
+    )
+
+    yield r
+    r.flushall()
+
+
+@pytest.fixture(name="mock_pipe")
+def mock_pipe(mocker):
+    mock_pipe = mocker.MagicMock()
+    yield mock_pipe
+
+
+@pytest.fixture(name="mock_r")
+def mock_redis(mocker, mock_pipe):
+    mock_r = mocker.MagicMock()
+    mock_r.pipeline.return_value = mock_pipe
+    yield mock_r
+
+
+@pytest.fixture(name="mock_user_id")
+def mock_user_id():
+    uid = "cce8594a-0e36-467d-9dd3-efe9c8376c94"
+    yield uid
--- api/tests/test_user_queries.py
+++ api/tests/test_user_queries.py
@@ -25,37 +25,37 @@ def mock_user():
     yield user
 
 
-@pytest.mark.integration
-@pytest.fixture(name="redis")
-def real_redis():
-    r = redis.Redis(
-        host=getenv("REDIS_HOST", ""),
-        port=getenv("REDIS_PORT", "6379"),
-        password=getenv("REDIS_PW", "devpassword"),
-        decode_responses=True,
-    )
+# @pytest.mark.integration
+# @pytest.fixture(name="redis")
+# def real_redis():
+#     r = redis.Redis(
+#         host=getenv("REDIS_HOST", ""),
+#         port=getenv("REDIS_PORT", "6379"),
+#         password=getenv("REDIS_PW", "devpassword"),
+#         decode_responses=True,
+#     )
 
-    yield r
-    r.flushall()
-
-
-@pytest.fixture(name="mock_pipe")
-def mock_pipe(mocker):
-    mock_pipe = mocker.MagicMock()
-    yield mock_pipe
+#     yield r
+#     r.flushall()
 
 
-@pytest.fixture(name="mock_r")
-def mock_redis(mocker, mock_pipe):
-    mock_r = mocker.MagicMock()
-    mock_r.pipeline.return_value = mock_pipe
-    yield mock_r
+# @pytest.fixture(name="mock_pipe")
+# def mock_pipe(mocker):
+#     mock_pipe = mocker.MagicMock()
+#     yield mock_pipe
 
 
-@pytest.fixture(name="mock_user_id")
-def mock_user_id():
-    uid = "cce8594a-0e36-467d-9dd3-efe9c8376c94"
-    yield uid
+# @pytest.fixture(name="mock_r")
+# def mock_redis(mocker, mock_pipe):
+#     mock_r = mocker.MagicMock()
+#     mock_r.pipeline.return_value = mock_pipe
+#     yield mock_r
+
+
+# @pytest.fixture(name="mock_user_id")
+# def mock_user_id():
+#     uid = "cce8594a-0e36-467d-9dd3-efe9c8376c94"
+#     yield uid
 
 
 @pytest.fixture()
@@ -72,7 +72,7 @@ def serialized_user(mock_user_id):
 
 
 @pytest.mark.integration
-def test_create_user_int(mocker, redis, mock_user, mock_user_id):
+def test_create_user_int(mocker, redis, mock_user, mock_user_id, serialized_user):
     mocker.patch("codespace_backend.queries.users.get_db", return_value=redis)
     mocker.patch(
         "codespace_backend.queries.users.generate_user_id", return_value=mock_user_id
@@ -80,7 +80,7 @@ def test_create_user_int(mocker, redis, mock_user, mock_user_id):
 
     user_id = create_user(mock_user)
     user = redis.hgetall(users_key(mock_user_id))
-    assert user == serialize(mock_user, mock_user_id)
+    assert user == serialized_user
 
     exists = redis.sismember(usernames_unique_key(), mock_user["username"])
     assert exists == True
