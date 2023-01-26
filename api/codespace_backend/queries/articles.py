53eab9a721da265fca79615863e949f0b2ce186d
Logan Figgins <l.figgins.se@gmail.com>
2023-01-26 14:10:57 -0800

    implement get_articles_by_creation_date redis query

--- api/codespace_backend/queries/articles.py
+++ api/codespace_backend/queries/articles.py
@@ -9,7 +9,7 @@ def create_article(article: dict, user_id: str) -> int:
     # article id
     article_id = gen_id()
     ts = get_utc_timestamp()
-    serialized = serialize(article, created_at=ts, user_id=user_id)
+    serialized = serialize(article, created_at=ts, user_id=user_id, art_id=article_id)
     pipe = r.pipeline()
     pipe.hset(
         articles_key(article_id),
@@ -21,11 +21,50 @@ def create_article(article: dict, user_id: str) -> int:
     return article_id
 
 
-def serialize(article: dict, created_at: int, user_id: str):
+# since this app's MVP only expects a single user we do not yet need define
+# data structures for queriying them by username, only by creation date for now
+def get_articles_by_creation_date(offset=0, count=10, desc=False):
+    r = get_db()
+    get = [
+        "#",
+        f"{articles_key('*')}->title",
+        f"{articles_key('*')}->description",
+        f"{articles_key('*')}->owner_id",
+        f"{articles_key('*')}->created_at",
+        f"{articles_key('*')}->code",
+        f"{articles_key('*')}->lang",
+    ]
+    ouput = r.sort(
+        article_by_create_at_key(),
+        start=offset,
+        num=count,
+        by="nosort",
+        get=get,
+        groups=True,
+    )
+    results = [
+        deserialize(
+            {
+                "id": id,
+                "title": title,
+                "description": description,
+                "owner_id": owner_id,
+                "created_at": created_at,
+                "code": code,
+                "lang": lang,
+            }
+        )
+        for id, title, description, owner_id, created_at, code, lang in ouput
+    ]
+
+    return results
+
+
+def serialize(article: dict, created_at: int, user_id: str, art_id: str):
     # don't mute article
     result = article.copy()
     code, lang = result.pop("codeSnippet").values()
-
+    result["id"] = art_id
     result["code"] = code
     result["lang"] = lang
     result["owner_id"] = user_id
@@ -40,7 +79,7 @@ def deserialize(article: dict) -> dict:
     INT_KEYS = {"created_at"}
 
     for k, v in article.items():
-        if v in INT_KEYS:
+        if k in INT_KEYS:
             v = int(v)
         if k in SNIPPET_KEYS:
             result["code_snippet"][k] = v
--- api/tests/test_article_queries.py
+++ api/tests/test_article_queries.py
@@ -1,16 +1,22 @@
 import pytest
 
 from codespace_backend.queries.keys import article_by_create_at_key, articles_key
-from codespace_backend.queries.articles import serialize, deserialize, create_article
+from codespace_backend.queries.articles import (
+    serialize,
+    deserialize,
+    create_article,
+    get_articles_by_creation_date,
+)
 
 
 @pytest.fixture()
 def serialized_article(mock_created_at, mock_uuid):
     yield {
-        "created_at": str(mock_created_at),
-        "owner_id": mock_uuid,
-        "description": "html code",
+        "id": mock_uuid,
         "title": "title",
+        "description": "html code",
+        "owner_id": mock_uuid,
+        "created_at": str(mock_created_at),
         "code": "<div><div>",
         "lang": "HTML",
     }
@@ -19,11 +25,12 @@ def serialized_article(mock_created_at, mock_uuid):
 @pytest.fixture()
 def deserialized_article(mock_created_at, mock_uuid):
     yield {
-        "created_at": mock_created_at,
-        "owner_id": mock_uuid,
-        "description": "html code",
+        "id": mock_uuid,
         "title": "title",
-        "contact_snippet": {"code": "<div><div>", "lang": "HTML"},
+        "description": "html code",
+        "owner_id": mock_uuid,
+        "created_at": mock_created_at,
+        "code_snippet": {"code": "<div><div>", "lang": "HTML"},
     }
 
 
@@ -34,6 +41,112 @@ def mock_article():
         "title": "title",
         "codeSnippet": {"code": "<div><div>", "lang": "HTML"},
     }
+
+
+@pytest.fixture()
+def mock_uuid_list():
+    yield [
+        "678fd3c1-5e75-419c-ac25-c0af12f9cf12",
+        "a05f45b3-a606-4636-b001-3875f49c5d85",
+        "e6c9baff-1d83-467c-b982-b02f059e882a",
+        "b114aae1-5c5e-4ad7-910f-4f302535dfcb",
+        "1e03273d-32f3-49e1-aa4e-15e03a24049e",
+    ]
+
+
+@pytest.fixture()
+def mock_article_list(serialized_article, mock_uuid_list):
+    articles = []
+    KEY_ORDER = [
+        "id",
+        "title",
+        "description",
+        "owner_id",
+        "created_at",
+        "code",
+        "lang",
+    ]
+
+    for i in range(5):
+        # do not mutate fixture
+        copy = serialized_article.copy()
+        copy["created_at"] = str(int(copy["created_at"]) + i * 100)
+        copy["id"] = mock_uuid_list[i]
+        # due to the way dictionaries, tuple unpacking,
+        # and the declared order of the sort command,
+        # ONLY when mocking i must reorder the keys
+
+        ordered = {k: copy[k] for k in KEY_ORDER}
+        articles.append(ordered)
+
+    print("articles", articles)
+
+    # due to the way dictionaries, tuple unpacking, and the declared order of the sort command, ONLY when mocking i must reorder the keys
+
+    yield articles
+
+
+@pytest.mark.integration
+@pytest.fixture(name="redis_with_data")
+def redis_with_article_data(redis, mock_article_list, mock_uuid_list):
+    for i, article in enumerate(mock_article_list):
+        redis.hset(articles_key(mock_uuid_list[i]), mapping=article)
+        redis.zadd(
+            article_by_create_at_key(),
+            mapping={mock_uuid_list[i]: int(article["created_at"])},
+        )
+
+    yield redis
+
+
+class TestGetArticles:
+    @pytest.mark.integration
+    def test_success_int(
+        self, mocker, redis_with_data, mock_article_list, mock_uuid_list
+    ):
+        mocker.patch(
+            "codespace_backend.queries.articles.get_db", return_value=redis_with_data
+        )
+
+        articles = get_articles_by_creation_date()
+        deserialized = list(map(deserialize, mock_article_list))
+        assert articles == deserialized
+
+        articles = get_articles_by_creation_date(offset=1, count=2)
+        assert articles == deserialized[1:3]
+
+    def test_success(
+        self,
+        mocker,
+        mock_r,
+        mock_article_list,
+    ):
+        mocker.patch("codespace_backend.queries.articles.get_db", return_value=mock_r)
+        mock_r.sort.return_value = [d.values() for d in mock_article_list]
+        print("mock_art_list", mock_article_list)
+        get = [
+            "#",
+            f"{articles_key('*')}->title",
+            f"{articles_key('*')}->description",
+            f"{articles_key('*')}->owner_id",
+            f"{articles_key('*')}->created_at",
+            f"{articles_key('*')}->code",
+            f"{articles_key('*')}->lang",
+        ]
+
+        deserialized = list(map(deserialize, mock_article_list))
+
+        articles = get_articles_by_creation_date()
+        assert articles == deserialized
+
+        mock_r.sort.assert_called_with(
+            article_by_create_at_key(),
+            start=0,
+            num=10,
+            by="nosort",
+            get=get,
+            groups=True,
+        )
 
 
 class TestCreateArticle:
@@ -103,5 +216,11 @@ class TestSerializers:
         self, serialized_article, mock_article, mock_uuid, mock_created_at
     ):
         assert serialized_article == serialize(
-            mock_article, created_at=mock_created_at, user_id=mock_uuid
+            mock_article,
+            created_at=mock_created_at,
+            user_id=mock_uuid,
+            art_id=mock_uuid,
         )
+
+    def test_deserialize(self, deserialized_article, serialized_article):
+        assert deserialized_article == deserialize(serialized_article)
