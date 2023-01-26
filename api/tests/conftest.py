ff9131ff74fcbdd7ec053d46c19bf7871e51c1c8
Logan Figgins <l.figgins.se@gmail.com>
2023-01-26 14:54:18 -0800

    move fixtures

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -39,3 +39,80 @@ def mock_uuid():
 @pytest.fixture(name="mock_created_at")
 def mock_created_at_ts():
     return 1674702339
+
+
+@pytest.fixture()
+def serialized_article(mock_created_at, mock_uuid):
+    yield {
+        "id": mock_uuid,
+        "title": "title",
+        "description": "html code",
+        "owner_id": mock_uuid,
+        "created_at": str(mock_created_at),
+        "code": "<div><div>",
+        "lang": "HTML",
+    }
+
+
+@pytest.fixture()
+def deserialized_article(mock_created_at, mock_uuid):
+    yield {
+        "id": mock_uuid,
+        "title": "title",
+        "description": "html code",
+        "owner_id": mock_uuid,
+        "created_at": mock_created_at,
+        "code_snippet": {"code": "<div><div>", "lang": "HTML"},
+    }
+
+
+@pytest.fixture()
+def mock_article():
+    yield {
+        "description": "html code",
+        "title": "title",
+        "codeSnippet": {"code": "<div><div>", "lang": "HTML"},
+    }
+
+
+@pytest.fixture()
+def mock_uuid_list():
+    yield [
+        "678fd3c1-5e75-419c-ac25-c0af12f9cf12",
+        "a05f45b3-a606-4636-b001-3875f49c5d85",
+        "e6c9baff-1d83-467c-b982-b02f059e882a",
+        "b114aae1-5c5e-4ad7-910f-4f302535dfcb",
+        "1e03273d-32f3-49e1-aa4e-15e03a24049e",
+    ]
+
+
+@pytest.fixture()
+def mock_article_list(serialized_article, mock_uuid_list):
+    articles = []
+    KEY_ORDER = [
+        "id",
+        "title",
+        "description",
+        "owner_id",
+        "created_at",
+        "code",
+        "lang",
+    ]
+
+    for i in range(5):
+        # do not mutate fixture
+        copy = serialized_article.copy()
+        copy["created_at"] = str(int(copy["created_at"]) + i * 100)
+        copy["id"] = mock_uuid_list[i]
+        # due to the way dictionaries, tuple unpacking,
+        # and the declared order of the sort command,
+        # ONLY when mocking i must reorder the keys
+
+        ordered = {k: copy[k] for k in KEY_ORDER}
+        articles.append(ordered)
+
+    print("articles", articles)
+
+    # due to the way dictionaries, tuple unpacking, and the declared order of the sort command, ONLY when mocking i must reorder the keys
+
+    yield articles
