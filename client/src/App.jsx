86a4fedba1134682e3dc5c1c423598eec78a4055
Chris Hendricks <crshenx@gmail.com>
2023-01-31 12:47:05 -0800

    init about me form skeleton

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./view/Header";
 import SignUp from "./view/SignUp";
+import AboutMe from "./view/AboutMe";
 function App() {
   return (
     <div className="">
       <Header />
       <SignUp />
+      <AboutMe />
     </div>
   );
 }
