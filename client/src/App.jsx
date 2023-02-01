7d075e22bef74263159894c7dd249c1355dcd95e
Chris Hendricks <crshenx@gmail.com>
2023-01-31 16:49:04 -0800

    browser router init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,16 @@ import "./App.css";
 import Header from "./view/Header";
 import SignUp from "./view/SignUp";
 import AboutMe from "./view/AboutMe";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="">
-      <Header />
-      <SignUp />
-      <AboutMe />
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="about" element={<AboutMe />} />
+      </Routes>
     </div>
   );
 }
