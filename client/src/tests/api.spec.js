6088ac4fb65fbe3b27f06e10ca6a8b33fc2ff000
Logan Figgins <l.figgins.se@gmail.com>
2023-02-09 18:28:44 -0800

    update imports

--- client/src/tests/api.spec.js
+++ client/src/tests/api.spec.js
@@ -1,11 +1,11 @@
-import axios from "axios";
-import API from "../api/api";
+import http from "../utils/http";
+import API from "../utils/api";
 import { beforeAll, beforeEach, describe, expect, it, vi } from "vitest";
 
 describe("API tests", () => {
   beforeAll(() => {
-    axios.post = vi.fn();
-    axios.get = vi.fn();
+    http.post = vi.fn();
+    http.get = vi.fn();
   });
   beforeEach(() => {
     vi.resetAllMocks();
@@ -16,27 +16,27 @@ describe("API tests", () => {
         username: "user",
         password: "pw123",
       };
-      const expectedEndPoint = `${API.PREFIX}/auth/register`;
-      axios.post.mockResolvedValue({
+      const expectedEndPoint = `/auth/register`;
+      http.post.mockResolvedValue({
         status: 200,
         data: { payload: "Post successful" },
       });
       const response = await API.createAdminUser(user);
       expect(response.data).toEqual({ payload: "Post successful" });
-      expect(axios.post).toHaveBeenCalledWith(expectedEndPoint, user);
+      expect(http.post).toHaveBeenCalledWith(expectedEndPoint, user);
     });
   });
   describe("getArticles", () => {
     it("should return an array of articles", async () => {
-      const expectedEndPoint = `${API.PREFIX}/articles`;
+      const expectedEndPoint = `/articles`;
       const expected = [
         { id: 1, data: {} },
         { id: 2, data: {} },
       ];
-      const axiosRes = { data: { payload: expected }, status: 200 };
-      axios.get.mockResolvedValue(axiosRes);
+      const httpRes = { data: { payload: expected }, status: 200 };
+      http.get.mockResolvedValue(httpRes);
       const articles = await API.getArticles();
-      expect(axios.get).toHaveBeenCalledWith(expectedEndPoint);
+      expect(http.get).toHaveBeenCalledWith(expectedEndPoint);
       expect(articles).toEqual(expected);
     });
   });
--- client/src/view/SignIn.jsx
+++ client/src/view/SignIn.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import api from "../api/api";
+import api from "../utils/api";
 
 function SignIn() {
   const [credentials, setcredentials] = useState({});
