ccfe8119e1a009a564fcffd6c874b6b62441c7c3
Logan Figgins <l.figgins.se@gmail.com>
2023-02-09 18:29:10 -0800

    Update publishArticle component

--- client/src/view/PublishArticle.jsx
+++ client/src/view/PublishArticle.jsx
@@ -1,20 +1,28 @@
 import React, { useState, useRef } from "react";
+import { camelCase, trim } from "lodash";
+import api from "../utils/api";
+import { Modal } from "../components/Modal";
 import MarkdownInput from "../components/MarkdownInput";
 import TextInputField from "../components/TextInput";
 import IndigoButton from "../components/StyledButton";
-import { camelCase, trim } from "lodash";
-import api from "../api/api";
 
 export default function PublishArticle() {
   const [md, setMd] = useState("");
   const [articleData, setArticleData] = useState("");
+  // as of react 18 all setState calls are batched
+  const [showModal, setShowModal] = useState(false);
+  const [notification, setNotification] = useState({
+    title: undefined,
+    msg: undefined,
+    type: undefined,
+  });
+
   const pubBtnRef = useRef(null);
 
   const handleChange = (e) => {
     setArticleData((data) => {
       return {
         ...data,
-        // [e.target.name]: e.target.value,
         [camelCase([e.target.name])]: trim(e.target.value),
       };
     });
@@ -22,7 +30,8 @@ export default function PublishArticle() {
 
   const handlePublishClick = (e) => {
     e.stopPropagation();
-    //disable button until api returns
+    // disable button until api returns
+    // should debounce? instead?
     if (pubBtnRef.current) {
       pubBtnRef.current.setAttribute("disabled", "disabled");
     }
@@ -30,41 +39,66 @@ export default function PublishArticle() {
     const payload = { ...articleData, markdown: md };
     api
       .createArticle(payload)
-      .then((res) => console.log(res))
-      .catch((err) => console.error(err))
+      .then((res) => {
+        console.log(res);
+        setShowModal(true);
+        setNotification({
+          title: "Success",
+          msg: "Article published successfully!",
+          type: Modal.OK,
+        });
+      })
+      .catch((err) => {
+        setShowModal(true);
+        setNotification({
+          title: "Ooops",
+          msg: err.message,
+          type: Modal.ERROR,
+        });
+      })
       .finally(() => {
         pubBtnRef.current.removeAttribute("disabled");
       });
   };
 
   return (
-    <div className="container mx-auto px-4">
-      <h1 className="flex justify-center text-lg">Publish Article</h1>
-      <TextInputField
-        title="title"
-        id="title"
-        placeholder="Article Title"
-        handleChange={handleChange}
+    <>
+      <div className="container mx-auto px-4">
+        <h1 className="flex justify-center text-lg">Publish Article</h1>
+        <TextInputField
+          title="title"
+          id="title"
+          placeholder="Article Title"
+          handleChange={handleChange}
+        />
+        <TextInputField
+          title="description"
+          id="description"
+          placeholder="Article Description"
+          handleChange={handleChange}
+        />
+        <MarkdownInput
+          md={md}
+          handleChange={(e) => {
+            e.stopPropagation();
+            setMd(e.target.value);
+          }}
+        />
+        <IndigoButton
+          ref={pubBtnRef}
+          id="publish"
+          text="Publish"
+          handleClick={handlePublishClick}
+        />
+      </div>
+      {/* Headless UI Dialog internally uses useEvent hook to optimize re-renders */}
+      <Modal
+        open={showModal}
+        title={notification.title}
+        msg={notification.msg}
+        type={notification.type}
+        setOpen={setShowModal}
       />
-      <TextInputField
-        title="description"
-        id="description"
-        placeholder="Article Description"
-        handleChange={handleChange}
-      />
-      <MarkdownInput
-        md={md}
-        handleChange={(e) => {
-          e.stopPropagation();
-          setMd(e.target.value);
-        }}
-      />
-      <IndigoButton
-        ref={pubBtnRef}
-        id="publish"
-        text="Publish"
-        handleClick={handlePublishClick}
-      />
-    </div>
+    </>
   );
 }
