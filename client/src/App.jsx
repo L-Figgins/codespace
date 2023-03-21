73845f23208637905c66e87c46cb3e729acb763e
Logan Figgins <l.figgins.se@gmail.com>
2023-03-21 16:27:07 -0700

    Begin implementing nav bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,27 @@
 import "./App.css";
-import Header from "./view/Header";
+import Home from "./view/Home";
 import SignUp from "./view/SignUp";
 import AboutMe from "./view/AboutMe";
 import SignIn from "./view/SignIn";
 import PublishArticle from "./view/PublishArticle";
+import ArticleListElement from "./view/ArticleFeed";
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Header />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="about" element={<AboutMe />} />
-        <Route path="signin" element={<SignIn />} />
-        <Route path="publish" element={<PublishArticle />} />
-      </Routes>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="about" element={<AboutMe />} />
+          <Route path="signin" element={<SignIn />} />
+          <Route path="publish" element={<PublishArticle />} />
+          <Route path="feed" element={<ArticleListElement />} />
+        </Routes>
+      </main>
     </div>
   );
 }
