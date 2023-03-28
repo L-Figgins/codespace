0ba5cb27fe01da9fb38983411c214a155ad022de
Logan Figgins <l.figgins.se@gmail.com>
2023-03-28 00:08:16 -0700

    start useFormValidation

---
+++ client/src/hooks/use-form-validation.js
@@ -0,0 +1,51 @@
+import { useFormData } from "./use-form-data";
+
+/**
+ * @callback ValidateFunction
+ * @param {Object} - formData
+ * @returns {Object}
+ */
+
+/**
+ *
+ * @param {Object} initialValue - intialFormState
+ * @param {ValidateFunction} validate - validate function
+ * @param {Function} submit - submit function
+ * @returns {Object} - obj containing
+ */
+export function useFromValidation(initialValue, validate, submit) {
+  const { formData, handleChange, loadInitialData } = useFormData;
+  const [errors, setErrors] = useState({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  useEffect(() => {
+    if (isSubmitting) {
+      const noErrors = Object.keys(errors).length === 0;
+      if (noErrors) {
+        onSubmit(form);
+      }
+      setIsSubmitting(false);
+    }
+  }, [errors]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const validationErrors = validate(formData);
+    setErrors(validationErrors);
+    setIsSubmitting(true);
+  }
+
+  function handleBlur() {
+    const validationErrors = validate(form);
+    setErrors(validationErrors);
+  }
+
+  return {
+    formData,
+    handleChange,
+    handleSubmit,
+    handleBlur,
+    loadInitialData,
+    errors,
+  };
+}
